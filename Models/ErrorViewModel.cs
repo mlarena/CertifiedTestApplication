bb551e9db2115b589b656e494d28d3e6ac0426b1
v_piskarev <piskarev.vladimir@burstroy.ru>
2026-05-20 15:56:32 +0300

    CertifiedTestApplication

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CertifiedTestApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
